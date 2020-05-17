a3ecff02d57c428dde43f5692ad5fb13c593f70c
Charles A. Hessifer <hessifer@users.noreply.github.com>
2020-05-16 22:18:33 -0400

    example code for working with classes and objects

---
+++ WorkingWithClassesObjects/BankAccount.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace WorkingWithClassesObjects
+{
+    public class BankAccount
+    {
+        public int Number { get; }
+        public string Owner { get; set; }
+        public decimal Balnce
+        {
+            get
+            {
+                decimal balance = 0;
+
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        private readonly Random r = new Random();
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            Owner = name;
+            Number = r.Next(1000, 200000000);
+            MakeDeposit(initialBalance, DateTime.Now, "initial deposit");
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive.");
+            }
+
+            if (Balnce - amount < 0)
+            {
+                throw new InvalidOperationException("Insufficient funds for this withdrawal.");
+            }
+
+            var withdrawal = new Transaction(amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive.");
+            }
+
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public List<Transaction> GetTransactions()
+        {
+            return allTransactions;
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+
+            report.AppendLine("Date\t\tAmount\tNote");
+            foreach (var transaction in allTransactions)
+            {
+                // Rows
+                report.AppendLine($"{transaction.Date}\t{transaction.Amount}\t{transaction.Notes}");
+            }
+            return report.ToString();
+        }
+    }
+}
---
+++ WorkingWithClassesObjects/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace WorkingWithClassesObjects
+{
+    class Program
+    {
+        static void Main()
+        {
+            var myAccount = new BankAccount("Charles A. Hessifer", (decimal)1204.33);
+
+            // Display the account id and balance of my account
+            Console.WriteLine($"Account Number: {myAccount.Number}");
+            Console.WriteLine($"Account Balance: {myAccount.Balnce:C}");
+
+            // Make a deposit in our account
+            DateTime date = DateTime.Now;
+            try
+            {
+                myAccount.MakeDeposit((decimal)111.00, date, "Money from Twitch");
+
+                // Display our balance after the deposit
+                Console.WriteLine($"Account Balance: {myAccount.Balnce:C}");
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine($"ERROR: {e}");
+            }
+
+            // Get Account Transaction History
+            Console.WriteLine($"\n\n{myAccount.GetAccountHistory()}");
+        }
+    }
+}
